8f74bb7d1504eb5ace854e300788162f14c37b35
Lion Ackermann <ackerm.lion@hotmail.de>
2018-02-12 17:58:19 +0100

    added flask settings file

---
+++ backend/settings.py
@@ -0,0 +1,16 @@
+DEBUG = True
+
+"""
+try:
+    if DEBUG:
+        pass
+except:
+    DEBUG = False
+"""
+
+MONGO_PORT = 27017
+MONGO_HOST = 'localhost'
+MONGO_DBNAME = 'bekiga'
+
+SECRET_KEY = 'SuchSecretMuchWow'
+ADMIN_PW = '1234567'
